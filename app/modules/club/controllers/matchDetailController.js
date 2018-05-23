1729e4fba6c2b5fccc888405663bdda1a331488e
Juan Mosquera <juan.mosquera@unifyglobal.com>
2018-05-22 22:42:24 -0500

    add video in match detail

--- app/modules/club/controllers/matchDetailController.js
+++ app/modules/club/controllers/matchDetailController.js
@@ -1,16 +1,22 @@
 define(['club/club','moment','../../auth/factories/authFactory','../../shared/factories/modalFactory','../../shared/factories/errorFactory'], function(club, moment){
   'use strict';
 
-  club.controller('club.matchDetailController',['$scope','$filter','$location','$auth','auth.authFactory',
+  club.controller('club.matchDetailController',['$scope','$filter','$location','$sce','$auth','auth.authFactory',
     'shared.modalFactory','shared.errorFactory','shared.imageFactory','FileUploader',
-    function ($scope,$filter,$location,$auth,authFactory,modalFactory,errorFactory,imageFactory,FileUploader) {
+    function ($scope,$filter,$location,$sce,$auth,authFactory,modalFactory,errorFactory,imageFactory,FileUploader) {
 
       $scope.match = {};
+      $scope.showVideo = false;
 
       $scope.subOption = 3;
+      $scope.videoSource = null;
 
       if(sessionStorage.getItem('match')) {
         $scope.match = JSON.parse(sessionStorage.getItem('match'));
+        if ($scope.match.url_detail) {
+          console.log('aaaaaa ===> ', getParameterByName('v', $scope.match.url_detail));
+          $scope.videoSource = $sce.trustAsResourceUrl('//www.youtube.com/embed/' +  getParameterByName('v',$scope.match.url_detail));
+        }
         getChanceList();
       }
 
@@ -34,6 +40,20 @@ define(['club/club','moment','../../auth/factories/authFactory','../../shared/fa
       $scope.showInfo = function(info) {
         modalFactory.showInfoListModalFactory(info);
       };
+
+      $scope.showVideoContainer = function() {
+        $scope.showVideo =! $scope.showVideo;
+      };
+
+      function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+      }
 
       $scope.goBack = function() {
         $scope.subOption = 3;
